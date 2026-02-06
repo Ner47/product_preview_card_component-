a69de9251cdd5890492ec369e356c4be01e87cb4
Ner47 <neogil28@gmail.com>
2026-02-06 21:04:57 +0300

    feat: add product article with grid style

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,23 @@
 import styles from './App.module.css';
+import { iconCart, imgProductDesk, imgProductMobile } from '../shared/assets';
 
 function App() {
   return (
     <main className={styles.page}>
-      <h1>Vite + React</h1>
+      <article className={styles.product}>
+        <picture>
+          <source srcSet={imgProductDesk} media='(min-width: 768px)'/>
+          <img className={styles.product__img} src={imgProductMobile} alt="A bottle of Chanel perfume" />
+        </picture>
+        <div className={styles.product__card}>
+          <span className={styles.product__teg}>PERFUME</span>
+          <h2 className={styles.product__title}>Gabrielle Essence Eau De Parfum</h2>
+          <p className={styles.product__text}>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <span className={styles.product__newPrice}>$149.99</span>
+          <span className={styles.product__oldPrice}>$169.99</span>
+          <button className={styles.product__button}><img src={iconCart} alt="icon cart" />Add to Cart</button>
+        </div>
+      </article>
     </main>
   )
 }
---
+++ src/shared/assets/index.js
@@ -0,0 +1,3 @@
+export {default as imgProductDesk } from './image-product-desktop.jpg';
+export {default as imgProductMobile } from './image-product-mobile.jpg';
+export {default as iconCart } from './icon-cart.svg';
