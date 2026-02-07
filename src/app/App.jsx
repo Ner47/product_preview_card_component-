d8dd31497ecd946c21aac5a28b25eea91594185b
Ner47 <neogil28@gmail.com>
2026-02-07 11:58:04 +0300

    style: add font and button color and font size

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,15 +6,18 @@ function App() {
     <main className={styles.page}>
       <article className={styles.product}>
         <picture>
-          <source srcSet={imgProductDesk} media='(min-width: 768px)'/>
+          <source srcSet={imgProductDesk} media='(min-width: 769px)'/>
           <img className={styles.product__img} src={imgProductMobile} alt="A bottle of Chanel perfume" />
         </picture>
         <div className={styles.product__card}>
           <span className={styles.product__teg}>PERFUME</span>
           <h2 className={styles.product__title}>Gabrielle Essence Eau De Parfum</h2>
           <p className={styles.product__text}>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
-          <span className={styles.product__newPrice}>$149.99</span>
-          <span className={styles.product__oldPrice}>$169.99</span>
+          <div className={styles.product__price}>
+            <span className={styles.product__newPrice}>$149.99</span>
+            <span className={styles.product__oldPrice}>$169.99</span>
+          </div>
+          
           <button className={styles.product__button}><img src={iconCart} alt="icon cart" />Add to Cart</button>
         </div>
       </article>
