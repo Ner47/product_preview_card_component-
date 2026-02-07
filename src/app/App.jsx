057dc0ffb50614db8f93c0a7739330ee2f80c014
Ner47 <neogil28@gmail.com>
2026-02-07 12:32:49 +0300

    fix: change overflow product card

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,18 +7,32 @@ function App() {
       <article className={styles.product}>
         <picture>
           <source srcSet={imgProductDesk} media='(min-width: 769px)'/>
-          <img className={styles.product__img} src={imgProductMobile} alt="A bottle of Chanel perfume" />
+          <img 
+            className={styles.product__img} 
+            src={imgProductMobile} 
+            alt='A bottle of Chanel perfume' 
+          />
         </picture>
+
         <div className={styles.product__card}>
-          <span className={styles.product__teg}>PERFUME</span>
+          <span className={styles.product__tag}>PERFUME</span>
+
           <h2 className={styles.product__title}>Gabrielle Essence Eau De Parfum</h2>
-          <p className={styles.product__text}>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+
+          <p className={styles.product__text}>
+            A floral, solar and voluptuous interpretation composed by 
+            Olivier Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          
           <div className={styles.product__price}>
             <span className={styles.product__newPrice}>$149.99</span>
-            <span className={styles.product__oldPrice}>$169.99</span>
+            <s className={styles.product__oldPrice}>$169.99</s>
           </div>
           
-          <button className={styles.product__button}><img src={iconCart} alt="icon cart" />Add to Cart</button>
+          <button type='button' className={styles.product__button}>
+            <img src={iconCart} alt='' aria-hidden='true' />
+            Add to Cart
+          </button>
         </div>
       </article>
     </main>
